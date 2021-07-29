925789d7343eee6a80978d3ef615e12aac173d26
SanFre10 <fregasantiago@gmail.com>
2021-07-29 13:06:11 -0300

    Principio reproductor

--- index.js
+++ index.js
@@ -5,30 +5,38 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "static")));
+
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { status: req.query.sta });
-});
 
+app.get("/", (req, res) => {
+    res.render("index.ejs", { status: req.query.sta, id: req.query.id });
+});
+app.post("/search", (req, res) => {
+    try {
+        id = ytdl.getURLVideoID(req.body.url)
+        return res.redirect(`/?id=${id}`);
+    } catch (error) {
+        return res.redirect("/?sta=error");
+    }
+})
 app.get("/download", async (req, res) => {
     try {
         var url = req.query.url;
         var format = req.query.format;
 
         if (ytdl.validateURL(url)) {
-            console.log(url, format);
             if (format == "mp3") {
-                res.set({ "Content-Type": "audio/mp3", "Content-Disposition": "attachment; filename=audio.mp3" });
+                res.set({ "Content-Type": "audio/mp3", "Content-Disposition": "attachment; filename=audio.mp3", "Location": "/?sta=ok" });
                 ytdl(url, { filter: "audioonly", quality: "highestaudio" })
                     .on("error", (err) => {
                         console.log(err);
                     }).pipe(res);
             } else if (format == "mp4") {
-                res.set({ "Content-Type": "video/mp4", "Content-Disposition": "attachment; filename=video.mp4" });
+                res.set({ "Content-Type": "video/mp4", "Content-Disposition": "attachment; filename=video.mp4" , "Location": "/?sta=ok"});
                 ytdl(url, { quality: "highest" }) 
                     .on("error", (err) => {
                         console.log(err);
--- static/script.js
+++ static/script.js
@@ -1,18 +1,47 @@
-urlInput = document.getElementById("URL-input");
+urlInput    = document.getElementById("URL-input");
 downloadMP4 = document.getElementById("dmp4");
 downloadMP3 = document.getElementById("dmp3");
-icon = document.getElementById("icon");
+search      = document.getElementById("search");
+videoId     = document.getElementById("videoId");
+icon        = document.getElementById("icon");
 
-downloadMP4.addEventListener("click", () => {
-    if (urlInput.value) {
-        icon.src = "https://www.drupal.org/files/issues/throbber_12.gif";
-        window.location.replace(`/download?url=${urlInput.value}&format=mp4`);
-    }
+downloadMP4.addEventListener("click", () => download("mp4"));
+downloadMP3.addEventListener("click", () => download("mp3"));
+search.addEventListener("click", () => {
+    fetch("/search",{ 
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({url:urlInput.value}),
+    }).then((response) => {
+        if(response.status === 200) {
+            window.location.replace(response.url);
+        }
+    });
 });
 
-downloadMP3.addEventListener("click", () => {
+download = (format) => {
     if (urlInput.value) {
         icon.src = "https://www.drupal.org/files/issues/throbber_12.gif";
-        window.location.replace(`/download?url=${urlInput.value}&format=mp3`);
+        icon.className = "progress"
+        window.location.replace(`/download?url=${urlInput.value}&format=${format}`);
     }
-});
+}
+
+var player;
+function onYouTubeIframeAPIReady() {
+    console.log(videoId.value);
+    player = new YT.Player('player', {
+    videoId: videoId.innerHTML,
+    events: {
+        //'onReady': onPlayerReady,
+        // 'onStateChange': onPlayerStateChange
+    }
+    });
+}
+
+// function onPlayerReady(event) {
+//   event.target.playVideo();
+// }
+
